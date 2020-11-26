c09de766efe0f78d02763d294cdd154c8e536a37
Rémi Felin <remi.felin@gmail.com>
2020-11-26 11:48:07 +0100

    starter import

---
+++ main/main.py
@@ -0,0 +1,9 @@
+# 1 - Lire les fichiers XML
+# 2 - Identifier les noms (fajitas, salad, ...) via NLTK avec NER
+# 3 - relevé le sens de la phrase avec l'adjectif ou l'état de la personne
+# 4 - Réseaux de neurones
+# 5 - Evaluation
+# 6 - C'est bieng !
+
+if __name__ == '__main__':
+    print("Hello !")
