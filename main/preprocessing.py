410ff06d43e73c84f72e2237feccdfab7500978b
Alexis Vighi <alexis.vighi@yahoo.fr>
2020-12-10 08:38:48 +0100

    Ajout de la fonction label_encode dans preprocessing

--- main/preprocessing.py
+++ main/preprocessing.py
@@ -1,35 +1,39 @@
+
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 """
 Created on Sun Nov 29 15:10:13 2020
 
 @author: ALEXIS
 """
 
+# Un texte -> tokenize -> ner
+# Peut-être le transformer en dataframe pour faciliter le travail
 
-#Un texte -> tokenize -> ner 
-#Peut-être le transformer en dataframe pour faciliter le travail
-import spacy
 import nltk
-from nltk.corpus import stopwords  
-
-
-
+from nltk.corpus import stopwords
+from scikit_learn import preprocessing
 
 def tok(doc, sw=False):
     tokenized = nltk.word_tokenize(doc)
-    if sw == True:
-        stop_words = set(stopwords.words('english'))  
-        doc = [word for word in tokenized if not word in stop_words]  
+    if sw:
+        stop_words = set(stopwords.words('english'))
+        doc = [word for word in tokenized if word not in stop_words]
         return doc
     else:
         doc = [word for word in tokenized]
         return doc
 
+
 def pos_tag(doc):
     doc = nltk.pos_tag(doc)
     return doc
 
-doc='roses are red'
-doc = tok(doc)
-doc = pos_tag(doc)
+
+
+def label_encode(column):
+    le = preprocessing.LabelEncoder()
+    le.fit(column)
+    le.transform(column)
+    
+    return column
